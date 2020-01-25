cdd86def8403cb77466fabad4fe50c152c805301
Átila Tintino <atilatintino@gmail.com>
2020-01-25 03:30:13 -0300

    Conectando API CNPJ

---
+++ force-app/main/default/aura/CNPJ/CNPJController.js
@@ -0,0 +1,6 @@
+({
+    ControllerBuscaCNPJ : function(component, event, helper) {
+        console.log("Chamou Controller do Busca CNPJ");
+        helper.HelperBuscaCNPJ(component, event, helper);
+    }
+})
